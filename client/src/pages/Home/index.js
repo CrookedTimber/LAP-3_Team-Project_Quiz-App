51a6010802bdbb6fc7472eb012d4c90db9de8ec3
Edgar Ruiz <edgarrruizl@gmail.com>
2022-07-18 17:45:37 +0100

    Added timer (non-functional); API call moved to Lobby; OngoingMatch receives questions/answers and renders them according to the round(question index)

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,23 +1,42 @@
-import QuizLogo from "../../assets/quiz-logo.png";
-import { Container } from "react-bootstrap";
-import "./Home.css";
+import QuizLogo from '../../assets/quiz-logo.png';
+import { Container } from 'react-bootstrap';
+import './Home.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { userActions } from '../../reducers';
 
 export default function Home() {
-    return (
-        <>
-            <article className="home-container">
-                
-                <Container className="home-inner-container">
-                    <Container className="welcome-div">
-                        <h2 className="home-h2a">There's a time for playing it safe ... </h2>
-                        <h2 className="home-h2b">and there's a time for ...</h2>
-                        <h1 className="home-h1">Risky Quizness!</h1>
-                    </Container>
-                    <Container className="quiz-logo-container">
-                        <img className="quiz-logo" src={QuizLogo} alt="quiz logo"></img>
-                    </Container>
-                </Container>
-            </article>
-        </>
-    )
+  const dispatch = useDispatch();
+
+  const username = useSelector((state) => state.user.username);
+
+  function setBen() {
+    dispatch(userActions.setUsername('Ben'));
+  }
+
+  function reset() {
+    dispatch(userActions.resetUsername());
+  }
+
+  return (
+    <>
+      <article className="home-container">
+        <Container className="home-inner-container">
+          <Container className="welcome-div">
+            <h1>{username}</h1>
+            {!username && <h3>Hello</h3>}
+
+            <button onClick={setBen}>Set username to Ben</button>
+            <button onClick={reset}>Reset username</button>
+
+            <h2 className="home-h2a">There's a time for playing it safe ...</h2>
+            <h2 className="home-h2b">and there's a time for ...</h2>
+            <h1 className="home-h1">Risky Quizness!</h1>
+          </Container>
+          <Container className="quiz-logo-container">
+            <img className="quiz-logo" src={QuizLogo} alt="quiz logo"></img>
+          </Container>
+        </Container>
+      </article>
+    </>
+  );
 }
--- client/src/store.js
+++ client/src/store.js
@@ -2,6 +2,8 @@ import { userSlice, matchSlice } from './reducers';
 
 import { configureStore } from '@reduxjs/toolkit';
 
-const store = configureStore({ reducer: {user: userSlice.reducer, match: matchSlice.reducer} });
+const store = configureStore({
+  reducer: { user: userSlice.reducer, match: matchSlice.reducer },
+});
 
 export default store;
