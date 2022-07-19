9326af7c8b5945f6dfc85e76d0fad11c6304b253
Ben <bentidball97@gmail.com>
2022-07-19 10:45:54 +0100

    host socket implementation

--- client/src/pages/Match/index.jsx
+++ client/src/pages/Match/index.jsx
@@ -5,6 +5,23 @@ export default function Match() {
   const username = useSelector((state) => state.user.username);
   const gameStarted = useSelector((state) => state.match.gameStart);
 
+  const socket = io.connect("http://localhost:3001");
+
+  useEffect(() => {
+    if(username){
+      socket.on('connect', () => {
+        const connectedPlayer = new Player(socket.id, username);
+        console.log(connectedPlayer);
+      });
+    }
+
+    // socket.on('recieve_message', (data) => {
+    //   alert(data.message);
+    // });
+
+  }, [socket])
+
+
   return (
     <>
       <h3>{`Username: ${username}`}</h3>
