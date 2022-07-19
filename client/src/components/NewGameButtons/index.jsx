c56338371536a508bbe6c22828fdf7151f57735d
Edgar Ruiz <edgarrruizl@gmail.com>
2022-07-19 09:59:08 +0100

    Added 404 route, changed Home to Play in Navbar, added shuffle function for answers array

--- client/src/components/NewGameButtons/index.jsx
+++ client/src/components/NewGameButtons/index.jsx
@@ -27,7 +27,7 @@ export default function NewGameButtons() {
       <h3 className="d-flex justify-content-center">{`Hello, ${username}`}</h3>
       <div className="d-flex justify-content-center">
         <Button className="w-25 m-2" onClick={startMatch}>
-          New Game
+          Host Game
         </Button>
         <br />
         <Button className="w-25 m-2" onClick={handleJoinGame}>
