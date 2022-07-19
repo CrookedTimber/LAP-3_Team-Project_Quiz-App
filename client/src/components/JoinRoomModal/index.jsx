6d6458d4da3f7fd63c2a278d66bbaf39f9fd79a7
Edgar Ruiz <edgarrruizl@gmail.com>
2022-07-19 11:51:42 +0100

    Added input control to form in JoinRoomModal

--- client/src/components/JoinRoomModal/index.jsx
+++ client/src/components/JoinRoomModal/index.jsx
@@ -3,15 +3,22 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { matchActions } from '../../reducers';
 import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 export default function JoinRoomModal() {
   const showJoinRoom = useSelector((state) => state.match.showJoinRoom);
   const dispatch = useDispatch();
+  const [roomInput, setRoomInput] = useState('')
 
   const handleClose = () => dispatch(matchActions.toggleShowJoinRoomModal());
 
+
+  function updateRoomInput(e) {
+    setRoomInput(e.target.value);
+  }
   const handleJoinRoom = (e) => {
     e.preventDefault();
+    console.log(roomInput)
   };
 
   return (
@@ -29,7 +36,7 @@ export default function JoinRoomModal() {
           <Modal.Body>
             <Form.Group className="mb-3">
               <Form.Label>Room Number</Form.Label>
-              <Form.Control type="number" placeholder="0000" autoFocus />
+              <Form.Control onChange={updateRoomInput} type="number" placeholder="0000" autoFocus />
             </Form.Group>
           </Modal.Body>
           <Modal.Footer>
