a77c3ee7f052ba057db077bb2dad89919a936cbe
Samantha Dorrell <samantha.dorrell@yahoo.co.uk>
2022-07-20 17:52:46 +0100

    moved NewGameButtons folder to match development branch & added styles

---
+++ src/components/NewGameButtons/index.jsx
@@ -0,0 +1,54 @@
+import { Button, Container } from 'react-bootstrap';
+import UserProfileCard from '../UserProfileCard';
+import JoinRoomModal from '../JoinRoomModal';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { matchActions, userActions } from '../../reducers';
+
+import "./NewGameButtons.css";
+
+export default function NewGameButtons() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  // const username = useSelector((state) => state.user.username);
+
+  function emptyUsername() {
+    localStorage.removeItem('username');
+    dispatch(userActions.resetUser());
+    navigate("/");
+  }
+
+  const handleJoinRoomModal = () => {
+    dispatch(matchActions.toggleShowJoinRoomModal());
+  };
+
+  function startMatch() {
+    navigate('/match');
+    dispatch(userActions.setHost());
+  }
+
+  return (
+    <>
+    <Container className="dash-container">
+        <UserProfileCard />
+      
+      <div className="d-flex justify-content-center">
+        
+        <Button className="btn w-25 m-2 shadow" onClick={startMatch}>
+          Host a Game
+        </Button>
+        <br />
+        <Button className="btn w-25 m-2 shadow" onClick={handleJoinRoomModal}>
+          Join a Game
+        </Button>
+        <JoinRoomModal />
+        <br />
+        <br />
+        <Button className="btn w-25 m-2 shadow" onClick={emptyUsername}>
+          Change Username
+        </Button>
+      </div>
+      </Container>
+    </>
+  );
+}
