7f0a36371e7f05845ebef20ab5d00397173b99f6
Samantha Dorrell <samantha.dorrell@yahoo.co.uk>
2022-07-22 12:31:35 +0100

    fixed box clipping over footer

--- src/components/Audio/index.js
+++ src/components/Audio/index.js
@@ -1,7 +1,5 @@
 import { Component } from "react";
 
-const bgMusic = "https://www.youtube.com/clip/UgkxJg-t4bFY2LuQx7w459hI4zUaUA_ar4jR";
-
 class MusicTheme extends Component {
     render() {
     return (
